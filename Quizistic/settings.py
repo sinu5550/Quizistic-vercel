6a23e131144f8a31b334611a9e5bd8749b644c16
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-03-01 16:29:09 +0600

    Vercel Deployment Made

--- Quizistic/settings.py
+++ Quizistic/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 LOGIN_URL = '/account/login/'
 # Base url to serve media files
