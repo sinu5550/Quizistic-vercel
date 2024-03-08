651a766776a124e63ac0a5f988d53c38525b41e9
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-03-08 19:35:23 +0600

    Deploy problem found

--- Quizistic/settings.py
+++ Quizistic/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'Quizistic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'izDhmtiCEEglpIkWtqUmBvqgsZOzsNLi',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '46469',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'izDhmtiCEEglpIkWtqUmBvqgsZOzsNLi',
+#         'HOST': 'monorail.proxy.rlwy.net',
+#         'PORT': '46469',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
